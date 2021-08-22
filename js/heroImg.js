0347301baf6e2f8968b245affe38140430922460
IGOR ALMEIDA DOS SANTOS <75753575+Igor-Almeidados-Santos@users.noreply.github.com>
2021-08-22 12:35:58 -0300

    Imagens de fundo
    Criando a função de Mudar as imagens de fundo.

--- js/heroImg.js
+++ js/heroImg.js
@@ -1,6 +1,25 @@
 const imgContainer = document.querySelector('.hero');
 const images = document.querySelectorAll('.image img');
+let cont = 0;
 
 setInterval(() => {
-    
-}, 8000)
+    changeImage();
+}, 8000);
+
+function changeImage () {
+    if(cont <= 2 ){
+        imgContainer.style.backgroundImage = "url(" + images[cont].src +")";
+    }else{
+        cont = 0;
+        imgContainer.style.backgroundImage = "url(" + images[cont].src +")";
+    };
+
+    cont++;
+};
+
+images.forEach(e => {
+    e.onclick = () =>{
+        imgContainer.style.backgroundImage = "url(" + e.src +")";
+    };
+});
+
